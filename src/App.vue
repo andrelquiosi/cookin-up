8aaf6c8fd25c6619dfc587e7d4dfa06258dbf8fd
André Luis Quiosi <andre.quiosi@outlook.com>
2024-03-04 21:46:13 -0300

    Template de seleção de Ingredientes e de conteúdo principal

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,17 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 export default {
-  components: { Banner }
+  components: { Banner, ConteudoPrincipal}
 }
 
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
 
-<style scoped>
-.banner {
-  padding: 4rem 7.5rem;
-  color: var(--creme);
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  column-gap: 3.25rem;
-}
-
-.logo {
-  height: 4.5rem;
-  margin-bottom: 3rem;
-}
-
-.frase-cabecalho {
-  margin-bottom: 2rem;
-}
-
-.texto-verde {
-  color: var(--verde-medio, #3D6D4A);
-}
-
-.foto-banner {
-  width: 35rem;
-}
-
-@media only screen and (max-width: 1300px) {
-  .banner {
-    padding: 4rem 3.75rem;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  .logo {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-  }
-}
-
-@media only screen and (max-width: 767px) {
-  .banner {
-    padding: 4rem 1.5rem;
-  }
-
-  .foto-banner {
-    width: min(100%, 21.25rem);
-  }
-}
-</style>
+<style scoped></style>
