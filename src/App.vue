ca3c2a45c1090a014bb0dcacfca7aca5a46559c2
André Luis Quiosi <andre.quiosi@outlook.com>
2024-05-14 15:47:09 -0300

    Recurso: Renderizar categorias de ingredientes usando componentes de cartão Introduziu um novo componente de cartão  (CardCategoria)  para exibir categorias de ingredientes dinamicamente. O componente busca dados de categoria e os renderiza em cartões individuais, aprimorando a interface do usuário e melhorando a organização dos ingredientes.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script lang="ts">
-import Banner from './components/Banner.vue';
-import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import Banner from "./components/Banner.vue";
+import ConteudoPrincipal from "./components/ConteudoPrincipal.vue";
 
 export default {
-  components: { Banner, ConteudoPrincipal}
-}
-
+  components: { Banner, ConteudoPrincipal },
+};
 </script>
 
 <template>
   <Banner />
   <ConteudoPrincipal />
 </template>
-
 
 <style scoped></style>
