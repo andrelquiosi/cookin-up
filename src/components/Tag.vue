79db56682b76489d39a99e612965661d50b4475a
André Luis Quiosi <andre.quiosi@outlook.com>
2024-05-14 17:43:04 -0300

    Aplicação de estilo digerente em um mesmo componente usado em diferentes lugares

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
 export default {
-  props: { texto: { type: String, required: true } },
+  props: { texto: { type: String, required: true }, ativa: Boolean },
 };
 </script>
 
 <template>
-  <span class="tag">
+  <span :class="['tag',{ ativa }]">
     {{ texto }}
   </span>
 </template>
@@ -18,6 +18,12 @@ export default {
   padding: 0.5rem;
   text-align: center;
   transition: 0.2s;
+  color: var(--cinza);
+  background: var(--cinza-claro);
+  font-weight: 400;
+}
+
+.tag.ativa {
   color: var(--creme, #fffaf3);
   background: var(--coral, #f0633c);
   font-weight: 700;
